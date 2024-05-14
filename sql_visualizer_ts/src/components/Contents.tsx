b9bea9095f9b52c94611663928ca784c68f22550
yo16 <xxyo1xx@gmail.com>
2024-05-14 15:03:49 +0900

    add react-dropzone

--- sql_visualizer_ts/src/components/Contents.tsx
+++ sql_visualizer_ts/src/components/Contents.tsx
@@ -1,4 +1,6 @@
 
+import { useState } from "react";
+
 import { QuerySelector } from "./QuerySelector";
 import { LineageCanvas } from "./LineageCanvas";
 import { DisplayCtrlPanel } from "./DisplayCtrlPanel";
@@ -6,11 +8,19 @@ import { DisplayCtrlPanel } from "./DisplayCtrlPanel";
 import "./Contents.css";
 
 function Contents() {
+    const [stmts, setStmts] = useState([]);
+
+    function handleOnChangeQuery(query: string) {
+        console.log(query);
+    }
+
     return (
         <div
             className="contents-container"
         >
-            <QuerySelector />
+            <QuerySelector
+                onChangedQuery={ handleOnChangeQuery }
+            />
             <LineageCanvas />
             <DisplayCtrlPanel />
         </div>
--- sql_visualizer_ts/src/components/QuerySelector.tsx
+++ sql_visualizer_ts/src/components/QuerySelector.tsx
@@ -1,7 +1,57 @@
+import { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
 
-function QuerySelector() {
+import "./QuerySelector.css";
+
+interface QuerySelectorProps {
+    onChangedQuery: (query: string) => void;
+}
+
+function QuerySelector({
+    onChangedQuery
+}: QuerySelectorProps) {
+
+    // onDrop
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+      // open if type is text/plain
+      acceptedFiles.forEach((f) => {
+        const reader = new FileReader();
+        reader.addEventListener("load", () => {
+            if (typeof reader.result === "string" ) {
+                console.log(`DROPPED! ${reader.result}`);
+            }
+        });
+        reader.readAsText(f, "utf-8");
+      })
+    }, []);
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
     return (
-        <div>query selector</div>
+        <div
+            className="query-selector-container"
+        >
+            {/* SQLファイルドロップゾーン */}
+            <div
+                {...getRootProps()}
+            >
+                <input {...getInputProps()} />
+                <div
+                    className={ "div-drop-zone " + (isDragActive?"div-drag-active":"") }
+                >
+                    Drop SQL-File
+                </div>
+            </div>
+
+            <div>
+                or
+            </div>
+
+            {/* 手入力ボタンゾーン */}
+            <div>
+                CLICK HERE and Enter SQL Query!
+            </div>
+        </div>
     );
 }
 
