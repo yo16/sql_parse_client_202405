fb8afd97372934a0e1ba4255204816205e273bb5
yo16 <xxyo1xx@gmail.com>
2024-05-14 14:13:19 +0900

    install tailwindcss, created Header

--- sql_visualizer_ts/src/components/Header.tsx
+++ sql_visualizer_ts/src/components/Header.tsx
@@ -1,7 +1,18 @@
 
+import { HeaderMenu } from "./HeaderMenu";
+
+import "./Header.css";
+
 function Header() {
     return (
-        <div>header</div>
+        <div className="header-container">
+            <div>
+                <h1 className="header-title">SQL Viewer</h1>
+            </div>
+            <div>
+                <HeaderMenu />
+            </div>
+        </div>
     );
 }
 
